f6703d311d8ddfa86a48f95d97d0459c839dacbb
Puthsarangsey LONG <long.puthsarangsey@ababank.com>
2025-04-27 19:06:59 +0700

    init project & map sdk

--- MCPMapDemo/MCPMapDemo/ContentView.swift
+++ MCPMapDemo/MCPMapDemo/ContentView.swift
@@ -1,55 +1,24 @@
+//
+//  ContentView.swift
+//  MCPMapDemo
+//
+//  Created by Puthsarangsey LONG on 27/4/25.
+//
+
 import SwiftUI
 
 struct ContentView: View {
-    @EnvironmentObject private var locationManager: LocationManager
-    @State private var showingSearchSheet = false
-    
     var body: some View {
-        ZStack(alignment: .top) {
-            // Map View container
-            MapContainerView()
-                .edgesIgnoringSafeArea(.all)
-            
-            // Top search bar
-            SearchBarView(showingSearchSheet: $showingSearchSheet)
-                .padding(.horizontal)
-                .padding(.top, 8)
-            
-            // Temperature indicator
-            TemperatureView(temperature: "16°")
-                .padding(.top, 100)
-                .padding(.trailing, 16)
-                .frame(maxWidth: .infinity, alignment: .trailing)
-            
-            // Navigation controls
-            MapControlsView()
-                .padding(.trailing, 16)
-                .padding(.top, 160)
-                .frame(maxWidth: .infinity, alignment: .trailing)
-                
-            // Home indicator area
-            if showingSearchSheet {
-                Color.black.opacity(0.4)
-                    .edgesIgnoringSafeArea(.all)
-                    .onTapGesture {
-                        withAnimation {
-                            showingSearchSheet = false
-                        }
-                    }
-                
-                SearchSheetView(isShowing: $showingSearchSheet)
-                    .transition(.move(edge: .bottom))
-            }
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
-        .sheet(isPresented: $showingSearchSheet) {
-            SearchSheetView(isShowing: $showingSearchSheet)
-        }
+        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-            .environmentObject(LocationManager())
-    }
+#Preview {
+    ContentView()
 }
--- MCPMapDemo/MCPMapDemo/MCPMapDemoApp.swift
+++ MCPMapDemo/MCPMapDemo/MCPMapDemoApp.swift
@@ -1,13 +1,17 @@
+//
+//  MCPMapDemoApp.swift
+//  MCPMapDemo
+//
+//  Created by Puthsarangsey LONG on 27/4/25.
+//
+
 import SwiftUI
 
 @main
 struct MCPMapDemoApp: App {
-    @StateObject private var locationManager = LocationManager()
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(locationManager)
         }
     }
 }
